afcefec87f24e416ee7157883e2fabef55631d97
Halamohamed <halaalimohammed@gmail.com>
2026-01-14 10:48:38 +0100

    Part1 done

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,88 @@
 package se.lexicon;
 
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static SubscriberFilter activeSubscriberFilter =subscriber -> subscriber.isActive();
+    private static SubscriberFilter expiringSubscribersFilter = subscriber -> subscriber.getMonthRemaining() <= 1;
+    private static SubscriberAction extendingSubscribersAction = subscriber -> subscriber.setMonthRemaining(subscriber.getMonthRemaining() + 2);
+    private static SubscriberAction markSubscriberInactiveAction = subscriber -> subscriber.setActive(false);
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SubscriberDAO subscriberDAO = new SubscriberDAO();
+        SubscriberProcessor processor = new SubscriberProcessor(subscriberDAO);
+        subscriberDAO.save(new Subscriber(1, "alic@email", Plan.BASIC, true, 5));
+        subscriberDAO.save(new Subscriber(2, "bobc@email", Plan.FREE, false,2));
+        subscriberDAO.save(new Subscriber(3, "carol@email", Plan.PRO, true,1));
+        subscriberDAO.save(new Subscriber(4, "dave@email", Plan.FREE, false,0));
+        List<Subscriber> activeSubscriber = processor.findSubscribers(subscriberDAO.findAll(), activeSubscriberFilter);
+
+        System.out.println("1- Active Subscribers:");
+        for (Subscriber subscriber: activeSubscriber){
+            System.out.println(subscriber);
+        }
+
+        List<Subscriber> expiringSubscribers = processor.findSubscribers(subscriberDAO.findAll(), expiringSubscribersFilter);
+        System.out.println("2- Expiring Subscribers:");
+        for (Subscriber subscriber: expiringSubscribers){
+            System.out.println(subscriber);
+        }
+
+        List<Subscriber> activeAndExpiringSubscribers =processor.findSubscribers(subscriberDAO.findAll(),
+                subscriber -> (
+            activeSubscriberFilter.matches(subscriber) && expiringSubscribersFilter.matches(subscriber)));
+
+        System.out.println("3- Active and Expiring Subscribers:");
+        for (Subscriber subscriber: activeAndExpiringSubscribers){
+            System.out.println(subscriber);
+        }
+        List<Subscriber> subscriberMatchesByPlan = processor.findSubscribers(subscriberDAO.findAll(),
+                subscriber -> subscriber.getPlan() == Plan.FREE);
+
+        System.out.println("4- Subscribers with FREE plan:");
+        for (Subscriber subscriber: subscriberMatchesByPlan){
+            System.out.println(subscriber);
+        }
+
+        List<Subscriber> payingSubscribers = processor.findSubscribers(subscriberDAO.findAll(),
+                subscriber -> subscriber.getPlan() == Plan.BASIC || subscriber.getPlan() == Plan.PRO);
+
+        System.out.println("5- Paying Subscribers (BASIC or PRO):");
+        for (Subscriber subscriber: payingSubscribers){
+            System.out.println(subscriber);
+        }
+
+        List<Subscriber> extendingSubscribers = processor.applyToMatching(subscriberDAO.findAll(), expiringSubscribersFilter,
+                extendingSubscribersAction);
+
+        System.out.println("6- Extending Expiring Subscribers by 2 months:");
+        for (Subscriber subscriber: extendingSubscribers){
+            System.out.println(subscriber);
+        }
+
+
+
+        List<Subscriber> markSubscriberInactive = processor.applyToMatching(subscriberDAO.findAll(), activeSubscriberFilter,
+                markSubscriberInactiveAction);
+
+        System.out.println("7- Marking Active Subscribers as Inactive:");
+        for (Subscriber subscriber: markSubscriberInactive){
+            System.out.println(subscriber);
+        }
+
+        System.out.println("___________________");
+        for (Subscriber subscriber: subscriberDAO.findAll()){
+            System.out.println("Final state of all subscribers:");
+            System.out.println(subscriber);
+        }
+        System.out.println("___________________");
+
+
     }
 }
